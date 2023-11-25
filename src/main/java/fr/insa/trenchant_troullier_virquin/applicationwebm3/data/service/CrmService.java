d34aba45b4353c1bbc4ead81043ddbb8e6fdb23c
Lypris <rudy.virquin@insa-strasbourg.fr>
2023-11-25 18:18:55 +0100

    ajout machines

--- src/main/java/fr/insa/trenchant_troullier_virquin/applicationwebm3/data/service/CrmService.java
+++ src/main/java/fr/insa/trenchant_troullier_virquin/applicationwebm3/data/service/CrmService.java
@@ -1,13 +1,7 @@
 package fr.insa.trenchant_troullier_virquin.applicationwebm3.data.service;
 
-import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.entity.Contact;
-import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.entity.Operateur;
-import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.entity.Statut;
-import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.entity.StatutOperateur;
-import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.repository.ContactRepository;
-import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.repository.OperateurRepository;
-import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.repository.StatusRepository;
-import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.repository.StatutOperateurRepository;
+import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.entity.*;
+import fr.insa.trenchant_troullier_virquin.applicationwebm3.data.repository.*;
 import fr.insa.trenchant_troullier_virquin.applicationwebm3.view.ConfirmationSuppressionView;
 import org.springframework.stereotype.Service;
 
@@ -20,17 +14,26 @@ public class CrmService {
     private final StatusRepository statusRepository;
     private final OperateurRepository operateurRepository;
     private final StatutOperateurRepository statutOperateurRepository;
+    private final MachineRepository machineRepository;
+    private final EtatPossibleMachineRepository etatPossibleMachineRepository;
+    private final EtatMachineRepository etatMachineRepository;
     private final ConfirmationSuppressionView confirmationView;
 
     public CrmService(ContactRepository contactRepository,
                       StatusRepository statusRepository,
                       OperateurRepository operateurRepository,
                       StatutOperateurRepository statutOperateurRepository,
+                        MachineRepository machineRepository,
+                        EtatPossibleMachineRepository etatPossibleMachineRepository,
+                        EtatMachineRepository etatMachineRepository,
                       ConfirmationSuppressionView confirmationView) {
         this.contactRepository = contactRepository;
         this.statusRepository = statusRepository;
         this.operateurRepository = operateurRepository;
         this.statutOperateurRepository = statutOperateurRepository;
+        this.machineRepository = machineRepository;
+        this.etatPossibleMachineRepository = etatPossibleMachineRepository;
+        this.etatMachineRepository = etatMachineRepository;
         this.confirmationView = confirmationView;
     }
 
@@ -136,4 +139,69 @@ public class CrmService {
         statutOperateurRepository.save(statutOperateur);
     }
 
+    //////////////////////////// MACHINE ////////////////////////////
+    public List<Machine> findAllMachines(String stringFilter) {
+        if (stringFilter == null || stringFilter.isEmpty()) {
+            return machineRepository.findAll();
+        } else {
+            return machineRepository.search(stringFilter);
+        }
+    }
+    public long countMachines() {
+        return machineRepository.count();
+    }
+
+    public void deleteMachine(Machine machine) {
+        List<EtatMachine> etatMachines = etatMachineRepository.findByMachine(machine);
+
+        if (etatMachines.isEmpty()) {
+            machineRepository.delete(machine);
+        } else {
+            deleteAssociatedEtatMachines(etatMachines);
+            machineRepository.delete(machine);
+        }
+    }
+
+    public void saveMachine(Machine machine) {
+        if (machine == null) {
+            System.err.println("Machine is null. Are you sure you have connected your form to the application?");
+            return;
+        }
+        machineRepository.save(machine);
+    }
+    private void deleteAssociatedEtatMachines(List<EtatMachine> etatMachines) {
+        for (EtatMachine etatMachine : etatMachines) {
+            etatMachineRepository.delete(etatMachine);
+        }
+    }
+    //////////////////////////// ETAT MACHINE ////////////////////////////
+    public List<EtatMachine> findAllEtatMachines(String stringFilter) {
+        if (stringFilter == null || stringFilter.isEmpty()) {
+            return etatMachineRepository.findAll();
+        } else {
+            return etatMachineRepository.search(stringFilter);
+        }
+    }
+    public void deleteEtatMachine(EtatMachine etatMachine) {
+        etatMachineRepository.delete(etatMachine);
+    }
+    public void saveEtatMachine(EtatMachine etatMachine) {
+        if (etatMachine == null) {
+            System.err.println("EtatMachine is null. Are you sure you have connected your form to the application?");
+            return;
+        }
+        etatMachineRepository.save(etatMachine);
+    }
+    //////////////////////////// ETAT POSSIBLE MACHINE ////////////////////////////
+    public List<EtatPossibleMachine> findAllEtatPossibleMachines(){
+        return etatPossibleMachineRepository.findAll();
+    }
+
+    public void saveEtatPossibleMachine(EtatPossibleMachine etatPossibleMachine) {
+        if (etatPossibleMachine == null) {
+            System.err.println("EtatPossibleMachine is null. Are you sure you have connected your form to the application?");
+            return;
+        }
+        etatPossibleMachineRepository.save(etatPossibleMachine);
+    }
 }
